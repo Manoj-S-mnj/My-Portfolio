29fb017a9f76262b8ca5b2c048556a3162e28486
Manoj S <142795422+Manoj-S-mnj@users.noreply.github.com>
2024-06-15 08:31:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var sidenav = document.querySelector(".side-navbar");
+sidenav.style.display="none";
+
+
+function showNavbar(){
+    sidenav.style.display="block";
+}
+
+function closeNavbar(){
+    sidenav.style.display="none";
+}
+
+// function showContent(val){
+//     var content = document.getElementById(val).innerHTML;
+//     document.querySelector("#home").innerHTML = content;
+// }
